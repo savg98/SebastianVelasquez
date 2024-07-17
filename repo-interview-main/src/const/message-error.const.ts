ed60c2fa830d84ec11ddd1cf419e1c7ca390aac6
Sebastian Velasquez <savg215@gmail.com>
2024-07-17 18:24:23 -0500

    initial commit

--- repo-interview-main/src/const/message-error.const.ts
+++
@@ -1,4 +0,0 @@
-export const MESSAGE_ERROR = {
-    'DuplicateIdentifier': "Duplicate identifier found in the database",
-    'NotFound': "Not product found with that identifier",
-}
--- repo-interview-main/src/interfaces/product.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface ProductInterface {
-  id: string;
-  name: string;
-  description: string;
-  logo: string;
-  date_release: Date;
-  date_revision: Date;
-}
