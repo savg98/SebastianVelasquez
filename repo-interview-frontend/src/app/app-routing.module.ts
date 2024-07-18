6588d1601c9081e80d231ee83fdd20351fcd07b4
Sebastian Velasquez <savg215@gmail.com>
2024-07-17 20:22:37 -0500

    products registration form added

--- repo-interview-frontend/src/app/app-routing.module.ts
+++ repo-interview-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsRegistrationFormComponent } from './pages/products-registration-form/products-registration-form.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      { path: "registrationForm", component: ProductsRegistrationFormComponent }
+    ]
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- repo-interview-frontend/src/app/pages/products-registration-form/products-registration-form.component.ts
+++ repo-interview-frontend/src/app/pages/products-registration-form/products-registration-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-products-registration-form',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsRegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  productsForm: FormGroup;
+
+  constructor() {
+    this.productsForm = new FormGroup({
+      id: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      logo: new FormControl('', Validators.required),
+      releaseDate: new FormControl('', Validators.required),
+      revisionDate: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit(): void {
   }
